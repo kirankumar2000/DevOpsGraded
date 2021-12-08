aeb04276967452824d67bbd2c461363eafcf8eb8
kirankumar2000 <kirankumar8017@gmail.com>
2021-12-09 00:50:25 +0530

    Initial commit

---
+++ src/main/java/com/jenkins/GradedAssignment10Application.java
@@ -0,0 +1,15 @@
+package com.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradedAssignment10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradedAssignment10Application.class, args);
+		
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
